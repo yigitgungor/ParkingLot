7dddac7cbdfffde9c25d3d89206752bc856e1551
Thanh Ngo <felix.infinite@gmail.com>
2017-09-30 23:02:58 -0400

    add Classes based on the UML design document

---
+++ src/edu/rutgers/cs431/teamchen/DepartCallback.java
@@ -0,0 +1,5 @@
+package edu.rutgers.cs431.teamchen;
+
+public interface DepartCallback {
+    public void depart(Car car);
+}
---
+++ src/edu/rutgers/cs431/teamchen/SyncClock.java
@@ -0,0 +1,11 @@
+package edu.rutgers.cs431.teamchen;
+
+public class SyncClock {
+    private long currentTime = 0L;
+
+    // TODO
+    public long getTime(){
+        return 0L;
+    }
+
+}
