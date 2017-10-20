6e7b4cf8001041aa0b236609da1f327cb4f962f4
muhammedelsayed <muhammed_elsayed@aol.com>
2017-10-20 15:05:07 -0400

    HttpServer, ParkingSpace, DepartureQueue

--- src/edu/rutgers/cs431/teamchen/DepartureQueue.java
+++ src/edu/rutgers/cs431/teamchen/DepartureQueue.java
@@ -1,5 +1,14 @@
 package edu.rutgers.cs431.teamchen;
 
 public class DepartureQueue {
-
+	
+	public DepartureQueue(){
+		Queue<Car> exitQueue = new Queue<Car>
+		
+	}
+	
+	public depart(Car c){
+		
+		
+	}
 }
