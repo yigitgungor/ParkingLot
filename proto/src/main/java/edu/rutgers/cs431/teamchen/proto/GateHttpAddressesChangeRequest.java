68eca85d9384c1c9d08b069735eb4a1ef367d067
Thanh Ngo <felix.infinite@gmail.com>
2017-10-22 13:15:06 -0400

    Update gate register request

--- proto/src/main/java/edu/rutgers/cs431/teamchen/proto/GateHttpAddressesChangeRequest.java
+++ proto/src/main/java/edu/rutgers/cs431/teamchen/proto/GateHttpAddressesChangeRequest.java
@@ -2,7 +2,12 @@ package edu.rutgers.cs431.teamchen.proto;
 
 import java.util.ArrayList;
 
-// Monitor -> all Gates: the monitor sends a new gate list to all gates when something changes
+// Monitor -> all Gates: the monitor sends a gate's http addresses to all gates when something changes.
+// The monitor expects no response.
+// (.ie after adding a new gate to the system)
 public class GateHttpAddressesChangeRequest {
+    // the gates' http addresses
     public ArrayList<String> gateHttpAddrs;
+    // the index of the gate receiving this request in the address list
+    public int index;
 }
